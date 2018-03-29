85c342eeb77d40b24bfe4fcc5a28642a4237d5b3
Jack Newcombe <jack.newcombe@just-eat.com>
2018-03-29 15:18:33 +0100

    Add unit tests for http methods

--- Example/Tests/HTTPClient.swift
+++ Example/Tests/HTTPClient.swift
@@ -16,8 +16,25 @@ typealias HTTPClientResult = (_ code: Int, _ response: String) -> Void
 class HTTPClient {
 
     static func get(url: String, headers: [String: String] = [:], completion: @escaping HTTPClientResult) {
+        execute(url: url, method: "GET", headers: headers, completion: completion)
+    }
+    
+    static func post(url: String, headers: [String: String] = [:], completion: @escaping HTTPClientResult) {
+        execute(url: url, method: "POST", headers: headers, completion: completion)
+    }
+
+    static func put(url: String, headers: [String: String] = [:], completion: @escaping HTTPClientResult) {
+        execute(url: url, method: "PUT", headers: headers, completion: completion)
+    }
+
+    static func delete(url: String, headers: [String: String] = [:], completion: @escaping HTTPClientResult) {
+        execute(url: url, method: "DELETE", headers: headers, completion: completion)
+    }
+
+    private static func execute(url: String, method: String = "GET", headers: [String: String] = [:], completion: @escaping HTTPClientResult) {
 		
         var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = method
         headers.forEach { request.addValue($0.value, forHTTPHeaderField: $0.key) }
 		let task = session.dataTask(with: request) { data, response, error in
 			let response = response as! HTTPURLResponse
