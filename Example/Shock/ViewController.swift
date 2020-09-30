8d663dac7c7106511c4a5bdac4fa199de196e7c3
Jack Newcombe <jack.newcombe@just-eat.com>
2020-09-30 13:30:12 +0100

    Tabs to spaces

--- Example/Shock/ViewController.swift
+++ Example/Shock/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
-	@IBOutlet var pickerView: UIPickerView!
-	@IBOutlet var scrollView: UIScrollView!
-	@IBOutlet var label: UILabel!
-	@IBOutlet var button: UIButton!
-	
-	let routes = MyRoutes()
+    
+    @IBOutlet var pickerView: UIPickerView!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    
+    let routes = MyRoutes()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		pickerView.dataSource = self
-		pickerView.delegate = self
+	    
+	    pickerView.dataSource = self
+	    pickerView.delegate = self
         
         button.layer.cornerRadius = 8.0
         scrollView.layer.cornerRadius = 8.0
@@ -30,9 +30,9 @@ class ViewController: UIViewController {
         pickerView.layer.borderColor = UIColor.gray.withAlphaComponent(0.5).cgColor
     }
 
-	@IBAction func performRequest(sender: UIButton) {
-		
-		routes.performRequest(index: pickerView.selectedRow(inComponent: 0)) { response, data, error in
+    @IBAction func performRequest(sender: UIButton) {
+	    
+	    routes.performRequest(index: pickerView.selectedRow(inComponent: 0)) { response, data, error in
             var text = ""
             if let error = error {
                 text += error.localizedDescription
@@ -43,32 +43,32 @@ class ViewController: UIViewController {
                 text += "\n"
                 text += String(data: data, encoding: .utf8) ?? ""
             }
-			DispatchQueue.main.async {
-				self.label.text = text
-				self.label.sizeToFit()
-				self.scrollView.contentOffset = CGPoint(x: 0, y: 0)
-			}
-		}
-		
-	}
+    	    DispatchQueue.main.async {
+	    	    self.label.text = text
+	    	    self.label.sizeToFit()
+	    	    self.scrollView.contentOffset = CGPoint(x: 0, y: 0)
+    	    }
+	    }
+	    
+    }
 
 }
 
 extension ViewController: UIPickerViewDataSource {
-	
-	func numberOfComponents(in pickerView: UIPickerView) -> Int {
-		return 1
-	}
-	
-	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-		return routes.count
-	}
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+	    return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+	    return routes.count
+    }
 }
 
 extension ViewController: UIPickerViewDelegate {
     
-	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-		return routes.nameOfRoute(at: row)
-	}
-	
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+	    return routes.nameOfRoute(at: row)
+    }
+    
 }
