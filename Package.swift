9245e20855099f938f15361f89c09c3b9eaa0261
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-06-26 15:11:26 +0100

    Remove unnecessary comments in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Shock",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Shock",
             targets: ["Shock"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio",
                  from: "2.40.0"),
         .package(name: "Mustache",
@@ -21,8 +18,6 @@ let package = Package(
                  from: "4.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Shock",
             dependencies: [
