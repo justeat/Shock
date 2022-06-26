b428acda0ebe3ad52991c44be64370fd16ec516f
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-06-26 17:37:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,34 +1,43 @@
-// swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Shock",
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "Shock",
-            targets: ["Shock"]),
+            targets: ["Shock"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio",
-                 from: "2.40.0"),
-        .package(name: "Mustache",
-                 url: "https://github.com/groue/GRMustache.swift",
-                 from: "4.0.1")
+                 exact: "2.40.0"),
+        .package(url: "https://github.com/groue/GRMustache.swift",
+                 exact: "4.0.1"),
+        .package(url: "https://github.com/justeat/JustLog",
+                 exact: "4.0.2")
     ],
     targets: [
         .target(
             name: "Shock",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
-                "Mustache"
+                .product(name: "Mustache", package: "GRMustache.swift")
             ],
-            path: "Shock/Classes/"),
+            path: "Framework/Sources"),
         .testTarget(
-            name: "Shock-Tests",
-            dependencies: ["Shock"],
-            path: "Example/Tests"),
+            name: "UnitTests",
+            dependencies: [
+                .byName(name: "Shock"),
+                .product(name: "JustLog", package: "JustLog")
+            ],
+            path: "Tests/Sources",
+            resources: [
+                .process("Resources")
+            ])
     ]
 )
